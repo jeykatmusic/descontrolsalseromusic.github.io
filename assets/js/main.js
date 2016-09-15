6ade0b98decac41ad52c5faedd61e2382171fb5c
nelsonandradeit <nelsonandradeit@gmail.com>
2016-09-15 16:42:41 -0500

    Cambios en los creditos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 /*
-	Eventually by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+	Artistas by NELSON ANDRADE TM
+	nelsonandrade.co | @nelsonandradeit
+	Free for personal and commercial use under the CCA 3.0 license (nelsonandrade.co/license)
 */
 
 (function() {
